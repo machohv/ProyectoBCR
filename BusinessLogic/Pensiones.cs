8b5596ea3744cf3235e63bc08c1ce6aa84cb7ab8
Oscar <oscarmarlon.chaves@ucrso.info>
2016-06-16 14:07:58 -0600

    Pensiones

---
+++ BusinessLogic/Pensiones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Pensiones
+    {
+        public int CodigoPension { get; set; }
+        public string Cedula { get; set; }
+        public string TipoPension { get; set; }
+        public double ValorPEnsion { get; set; }
+        
+        public Entidad Entidad { get; set; }
+        public Persona Persona { get; set; }
+
+
+
+    }
+}
